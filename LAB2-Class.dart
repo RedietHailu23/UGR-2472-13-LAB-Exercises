43774f1c4afe068a29967ee2ab815b227a02382a
Rediet Hailu <128184501+RedietHailu23@users.noreply.github.com>
2024-03-05 06:46:12 -0800

    answer for lab 2 class question

---
+++ LAB2-Class.dart
@@ -0,0 +1,22 @@
+class Car {
+  String brand;
+  String model;
+  int year;
+
+  Car(this.brand, this.model, this.year);
+
+  void makeSound() {
+    print('Vroom Vroom');
+  }
+}
+
+void main() {
+
+  Car myCar = Car('Toyota', 'Corolla', 2022);
+
+  print('Brand: ${myCar.brand}');
+  print('Model: ${myCar.model}');
+  print('Year: ${myCar.year}');
+
+  myCar.makeSound();
+}
